3eab7933002194d20b709fdce7ffe5e9a89cf1e8
Jorge Cardoso <jorge.cardoso@vr.com.br>
2025-01-13 09:59:24 -0300

    alterando log para validar implantacao do rollback

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello, ArgoCD!');
+  res.send('Hello, Rollback!');
 });
 
 const PORT = process.env.PORT || 3000;
